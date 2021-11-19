f4006341ce70b09c1ceeda92efb99aedbc7274ba
programacionDAMVC <programacionDAMVC@gmail.com>
2021-11-19 10:14:01 +0100

    modificado:     colecciones1/teoria/Arrays4.java 	modificado:     colecciones1/teoria/Listas1.java

--- colecciones1/teoria/Arrays4.java
+++ colecciones1/teoria/Arrays4.java
@@ -13,7 +13,22 @@
 public class Arrays4 {
 	
 	public static void main (String[] args) {
-		
+		double[][][] array = { 
+						{{1.1, 2}, {3}, {4.4, 6.6, 7.7}},
+						{ {2, 5, -6, 3.25}, {8}},
+						{ {1, 2 , 3.4, 5} }
+					   };
+		mostrarArrayTridimensional(array);
+	}
+	
+	public static void mostrarArrayTridimensional(double[][][] arrayT) {
+		for (int i = 0; i < arrayT.length; i++){
+			for (int j = 0; j < arrayT[i].length; j++) {
+				for (int k = 0; k < arrayT[i][j].length; k++){
+					System.out.printf("(%d, %d, %d) vale %.2f%n", i, j ,k, arrayT[i][j][k]);
+				}
+			}
+		}
 	}
 }
 
--- colecciones1/teoria/Listas1.java
+++ colecciones1/teoria/Listas1.java
@@ -18,11 +18,47 @@ public class Listas1 {
 		}
 		System.out.println(Arrays.toString(sArray));
 		
-		ArrayList<String> listaCadenas = new ArrayList<>();
+		ArrayList<String> listaCadenas = new ArrayList<>(); //crea lista vacía
+		System.out.printf("Tamaño de la lista %d%n", listaCadenas.size());
+		//añadir elementos a la lista
+		listaCadenas.add("ROSA");
+		listaCadenas.add("JOAQUÍN");
+		System.out.println(listaCadenas);
+		//añadimos mas datos a las colecciones
+		listaCadenas.add("ENRIQUE");
+		//??? Y al array
+		//sArray[5] = "enrique"; no se puede aumentar el tamaño del array
+		listaCadenas.add("ROMERO"); //si el de la lista
+		System.out.println(listaCadenas);
+
+		//Recorremos las listas:
+		//usamos un bucle fori
+		for (int i = 0; i < listaCadenas.size(); i++) {
+			System.out.printf("Posición: %d, valor: %s%n", i, listaCadenas.get(i));
+		}
+		//usando un bucle mejorado:
+		for (String nombre : listaCadenas) {
+			System.out.println(nombre);
+		}
 		
-		
-		
-		
+		//Queremos saber si la lista contiene un dato		
+		System.out.printf("¿Está ENRIQUE? %b%n", listaCadenas.contains("ENRIQUE"));
+		//lo mismo con un array
+		String nombreABuscar = "luisa";
+		for (String nombre : sArray ) {
+			if (nombre == nombreABuscar){
+				System.out.printf("%s está en el array%n", nombreABuscar);
+				break; //no hace falta seguir, hemos encontrado el nombre
+			}
+		}
+		System.out.println("\n----------------------");
+		System.out.println(listaCadenas);
+		//Eliminamos ENRIQUE de la lista
+		System.out.printf("¿Se ha eliminado ENRIQUE de lalista? %b%n", 
+								listaCadenas.remove("ENRIQUE"));
+		System.out.println(listaCadenas);
+		System.out.printf("¿Se ha eliminado ENRIQUE de lalista? %b%n", 
+								listaCadenas.remove("ENRIQUE"));
 		
 
 	}
